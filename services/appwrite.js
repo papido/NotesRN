87f7a62f90f6f3de343f0791928fb937335e4c53
Fauzan <heavenzownz@gmail.com>
2025-03-23 16:27:41 +0800

    Initial commit

---
+++ services/appwrite.js
@@ -0,0 +1,23 @@
+import { Client, Databases, Account } from 'react-native-appwrite';
+import { Platform } from 'react-native';
+
+const config = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    db: process.env.EXPO_PUBLIC_APPWRITE_DB_ID,
+    col: {
+        notes: process.env.EXPO_PUBLIC_APPWRITE_COL_NOTES_ID
+    }
+};
+
+const client = new Client()
+    .setEndpoint(config.endpoint)
+    .setProject(config.projectId)
+    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PACKAGE_NAME)
+
+
+const database = new Databases(client);
+
+const account = new Account(client)
+
+export { database, config, client, account };
